23d1cad240b0d7f9b61aca63cfc86352a9ff7ff0
Jazzyst <andriy.zhuravlov@gmail.com>
2017-04-13 22:04:44 +0300

    added quiz-markup

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -9,6 +9,13 @@
 			function QuizController(quizMetrics, DataService){
 				var vm = this;
 				vm.quizMetrics = quizMetrics;
+				vm.dataService = DataService;
+				vm.questionAnswered = questionAnswered;
+				vm.activeQuestion = 0;
+
+				function questionAnswered(){
+					
+				}
 			}
 
 })();
