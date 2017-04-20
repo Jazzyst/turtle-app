34df51327da0b69b89834469b46b62554806588b
Jazzyst <andriy.zhuravlov@gmail.com>
2017-04-20 20:31:03 +0300

    markup of results

---
+++ js/controllers/results.js
@@ -0,0 +1,14 @@
+(function(){
+	angular
+        .module("turtleFacts")
+        .controller("resultsCtrl", ResultsController);
+
+		ResultsController.$inject = ['quizMetrics', 'DataService'];
+
+		function ResultsController(quizMetrics, DataService){
+			var vm = this;
+
+			 vm.quizMetrics = quizMetrics;
+			 vm.dataService = DataService;
+		}
+})();
